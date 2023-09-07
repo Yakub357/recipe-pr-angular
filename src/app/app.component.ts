07c92c7970ac8484df51a0ac1959382fcf4985f6
Yakub Dev <yakub@sagta.com>
2023-09-07 14:57:20 +0800

    navigation completed, recipes and shopping-list components displaying according navigation links clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  loadedPage: string = 'recipes';
+
+  onNavigate(page: string) {
+    this.loadedPage = page;
+  }
+}
