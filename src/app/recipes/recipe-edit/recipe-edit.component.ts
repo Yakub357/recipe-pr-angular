13232bf3583d596909e8b6b2a94a8edbe147a952
Coding Raccoon <egamyakub357@gmail.com>
2023-10-27 15:50:13 +0800

    Adding New Ingredient Controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -26,6 +26,15 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl(),
+      })
+    );
+  }
+
   private initForm() {
     let recipeName: string = '';
     let recipeImgPath: string = '';
