e845eab495a2a8e422e76bb735a0f4f5cca6c537
Coding Raccoon <egamyakub357@gmail.com>
2023-10-30 15:18:15 +0800

    Deleting Ingredients and some finishing touches!

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -99,4 +99,8 @@ export class RecipeEditComponent implements OnInit {
   get controls() {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
+
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
 }
